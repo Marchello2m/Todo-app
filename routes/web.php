5306c3ea5abfe5c4db016b52eac99b1f6bcfed08
Marchello2m <marchello2m@gmail.com>
2021-11-07 21:14:44 +0200

    Gandriz Atjaunots

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+
 use App\Http\Controllers\TaskController;
 use App\Models\Category;
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,7 +26,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 
-
 Route::resource('tasks', TaskController::class);
 Route::post('tasks/{task}/complete',[TaskController::class,'complete'])
     ->middleware('auth')
@@ -33,5 +34,27 @@ Route::post('tasks/{task}/unComplete',[TaskController::class,'unComplete'])
     ->middleware('auth')
     ->name('tasks.unComplete');
 
+
+Route::get('/dumpster', function () {
+    return view('tasks.dumpster');
+});
+
+Route::get('/tasks/{id}/restore',function (int $id){
+    $task=\App\Models\Task::withTrashed()->find($id);
+    $task->restore();
+    return $task;
+});
+Route::get('/dumpster',function (){
+
+    $tasks =Task::onlyTrashed()->get();
+    foreach($tasks as $task){
+        echo "----------<br>";
+        echo $task->title."<br>";
+
+        echo $task->content."<br>";
+        echo "----------<br>";
+    }
+    return view('tasks.dumpster');
+});
 
 require __DIR__ . '/auth.php';
